e61914c1aad8985c433505627fd93287fde618a7
oywq2020 <2603321762@qq.com>
2022-09-25 17:16:44 +0800

    complete InputSystem and change the location of City to Art

---
+++ Assets/_Core/Scripts/Manager/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using _Core.Drove.Script.System;
+using _Core.Scripts;
+using QFramework;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour, IController
+{
+    /// <summary>
+    /// Used with InputSystem
+    /// </summary>
+    private IInputSystem inputSystem;
+    void Start()
+    {
+        inputSystem = this.GetSystem<IInputSystem>();
+        inputSystem.RegisterGetKeyDown(ClientKeys.Mouse0,(() =>
+        {
+            Debug.Log("000000000");
+        }));
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.anyKeyDown)
+        {
+            foreach (ClientKeys VARIABLE in Enum.GetValues(typeof(ClientKeys)))
+            {
+                if (Input.GetKeyDown((KeyCode)VARIABLE))
+                {
+                    if (inputSystem.KeyQueryDic.TryGetValue(VARIABLE,out List<Action> actions))
+                    {
+                        foreach (var action in actions)
+                        {
+                            action.Invoke();
+                        }
+                    }
+                }
+            }
+        }
+
+        if (Input.anyKey)
+        {
+            foreach (ClientKeys VARIABLE in Enum.GetValues(typeof(ClientKeys)))
+            {
+                if (Input.GetKey((KeyCode)VARIABLE))
+                {
+                    if (inputSystem.KeyQueryDicDown.TryGetValue(VARIABLE,out List<Action> actions))
+                    {
+                        foreach (var action in actions)
+                        {
+                            action.Invoke();
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+
+    public IArchitecture GetArchitecture()
+    {
+        return DroneArchitecture.Interface;
+    }
+}
--- Assets/_Core/Scripts/System/InputSystem.cs
+++ Assets/_Core/Scripts/System/InputSystem.cs
@@ -1,19 +1,103 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using _Core.Drove.Event;
 using QFramework;
+using UnityEngine;
+
 namespace _Core.Drove.Script.System
 {
+    public  enum ClientKeys
+    {
+        Mouse0 = KeyCode.Mouse0,
+        Mouse1 = KeyCode.Mouse1,
+    }
     public interface IInputSystem:ISystem
     {
+
+        Dictionary<ClientKeys, List<Action>> KeyQueryDic { get; }
+        Dictionary<ClientKeys, List<Action>> KeyQueryDicDown { get; }
+        void RegisterGetKey(ClientKeys keyCode, Action action);
+        void UnRegisterGetKey(ClientKeys keyCode, Action action);
+        void RegisterGetKeyDown(ClientKeys keyCode, Action action);
+        void UnRegisterGetKeyDown(ClientKeys keyCode, Action action);
         void InterruptDroneOperation(bool isInterrupt);
     }
+
+
+   
     public class InputSystem: AbstractSystem,IInputSystem
     {
+        /// <summary>
+        /// client customize the input buttion
+        /// </summary>
+      
+        public List<KeyCode> KeyCodes = new List<KeyCode>();
+
+        public Dictionary<ClientKeys, List<Action>> KeyQueryDic { get; } = 
+            new Dictionary<ClientKeys, List<Action>>();
+
+        public Dictionary<ClientKeys, List<Action>> KeyQueryDicDown { get; } =
+            new Dictionary<ClientKeys, List<Action>>();
         
         protected override void OnInit()
         {
+
            
         }
+
+        public void RegisterGetKey(ClientKeys keyCode, Action action)
+        {
+            //RegisterGetKey Event
+            if ( KeyQueryDic.TryGetValue(keyCode,out List<Action> actions))
+            {
+               actions.Add(action);
+            }
+            else 
+            {
+                var list = new List<Action>();
+                list.Add(action);
+                KeyQueryDic.Add(keyCode,list);
+            }
+        }
+
+        public void UnRegisterGetKey(ClientKeys keyCode, Action action)
+        {
+            //RegisterGetKey Event
+            if ( KeyQueryDic.TryGetValue(keyCode,out List<Action> actions))
+            {
+                actions.Remove(action);
+            }
+        }
+        
+        public void RegisterGetKeyDown(ClientKeys keyCode, Action action)
+        {
+            //RegisterGetKey Event
+            if ( KeyQueryDicDown.TryGetValue(keyCode,out List<Action> actions))
+            {
+                actions.Add(action);
+            }
+            else 
+            {
+                var list = new List<Action>();
+                list.Add(action);
+                KeyQueryDicDown.Add(keyCode,list);
+            }
+        }
+
+        public void UnRegisterGetKeyDown(ClientKeys keyCode, Action action)
+        {
+            //RegisterGetKey Event
+            if ( KeyQueryDicDown.TryGetValue(keyCode,out List<Action> actions))
+            {
+                actions.Remove(action);
+            }
+        }
+        
+        
+        //Interruption Method
+      
+
         public void InterruptDroneOperation(bool isInterrupt)
         {
             this.SendEvent(new DroneOperationListener(){IsInterrupt = isInterrupt});
